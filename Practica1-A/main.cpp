2b8e784a10b63f7cf1afa23c6de1eb075f7aecdd
Juan Miguel De Haro Ruiz <juan.miguel.de.haro@c6s301pc31.fib.upc.es>
2016-11-07 13:10:12 +0100

    asdsada

--- Practica1-A/main.cpp
+++ Practica1-A/main.cpp
@@ -2,6 +2,42 @@
 #include <fstream>
 #include "kshingleset.h"
 #include "minhashsignatures.h"
+#include <list>
+
+void generadorTextos(const string& nombre) {
+    srand(time(NULL));
+    ifstream input("../" + nombre + ".txt");
+
+    input.seekg(0, ios::end);
+    uint size = input.tellg();
+
+    input.seekg(0, ios::beg);
+
+    char* text = new char[size];
+    input.read(text, size);
+
+    char* permutacion = new char[size];
+
+    for(uint i = 0; i < 20 ;++i) {
+        list<uint> indices;
+        for(uint j = 0; j < size; ++j) indices.push_back(j);
+        for(uint j = 0; j < size; ++j) {
+            uint indiceRandom = rand()%indices.size();
+            list<uint>::iterator iterador = indices.begin();
+            for(uint k = 0; k <indiceRandom; ++k) {
+                ++iterador;
+            }
+            permutacion[*iterador] = text[j];
+            indices.erase(iterador);
+        }
+
+        ofstream output("../" + nombre + "Random" + to_string(i) + ".txt");
+        output.write(permutacion, size);
+        output.close();
+    }
+
+
+}
 
 void leeString(string& text) {
     cout << "Introduce la string a analizar" << endl;
@@ -34,46 +70,42 @@ void testHashKShingles() {
     cout << "Resultado: " << KShingle::hashKShingle(text) << endl;
 }
 
-void testMinHash() {
-    ifstream in1("../text1.txt");
-    ifstream in2("../text2.txt");
+void testMinHash(uint numText, const string& name) {
+    vector<char*> texts(numText);
+    vector<uint> textSize(numText);
 
-    in1.seekg(0, ios::end);
-    in2.seekg(0, ios::end);
+    for (uint i = 0; i < numText; ++i) {
+        ifstream input("../" + name + to_string(i) + ".txt");
 
-    uint size1 = in1.tellg();
-    uint size2 = in2.tellg();
+        input.seekg(0, ios::end);
+        uint size = input.tellg();
+        input.seekg(ios::beg);
 
-    cout << "texto 1 size: " << size1 << " texto 2 size: " << size2 << endl;
+        char* text = new char[size];
+        input.read(text, size);
 
-    in1.seekg(0, ios::beg);
-    in2.seekg(0, ios::beg);
+        texts[i] = text;
+        textSize[i] = size;
 
-    char* text1 = new char[size1];
-    in1.read(text1, size1);
-
-    char* text2 = new char[size2];
-    in2.read(text2, size2);
+        input.close();
+    }
 
     cout << "Introduce la k deseada." << endl;
     int k;
     cin >> k;
-    cout << "K es " << k << endl;
     cout << "Introduce t" << endl;
     uint t;
     cin >> t;
-    KShingleSet kshingleset1(k, text1, size1);
-    KShingleSet kshingleset2(k, text2, size2);
+    KShingleSet kshingleset1(k, texts[0], textSize[0]);
+    KShingleSet kshingleset2(k, texts[1], textSize[1]);
     cout << "El coeficiente de jaccard es con la manera guay " << kshingleset1.jaccard(kshingleset2) << endl;
 
-    vector<char*> textos = {text1, text2};
-    vector<uint> textSize = {size1, size2};
-    MinHashSignatures minHashSignatures(t, k, textos, textSize);
-    cout << "El coeficiente de jaccard es con la manera NO guay "  <<    minHashSignatures.jaccard(0,1) << endl;
+    MinHashSignatures minHashSignatures(t, k, texts, textSize);
+    cout << "El coeficiente de jaccard es con la manera NO guay "  <<    minHashSignatures.jaccard(0, 1) << endl;
 
 }
 
 int main() {
-    testMinHash();
+    testMinHash(2, "lorem");
 }
 
--- Practica1-A/minhashsignatures.cpp
+++ Practica1-A/minhashsignatures.cpp
@@ -22,6 +22,7 @@ MinHashSignatures::MinHashSignatures(uint t, uint k, vector<string> texts) {
             }
         }
         ++j;
+
     }
 
     for (uint i = 0; i < t; ++i) {
@@ -79,33 +80,94 @@ double MinHashSignatures::jaccard(uint a, uint b) {
     return double(sum)/signatures.size();
 }*/
 
+bool isPrime(uint number) {
+
+    if (number == 2 || number == 3)
+        return true;
+
+    if (number % 2 == 0 || number % 3 == 0)
+        return false;
+
+    uint divisor = 6;
+    while (divisor * divisor - 2 * divisor + 1 <= number) {
+
+        if (number % (divisor - 1) == 0)
+            return false;
+
+        if (number % (divisor + 1) == 0)
+            return false;
+
+        divisor += 6;
+
+    }
+
+    return true;
+
+}
+
+void garbell(vector<bool>& v){
+    v[0] = false;
+    v[1] = false;
+    for(uint i = 2; i*i <= v.size(); ++i){
+        if(v[i]){
+            for(uint j = 2*i;j < v.size(); j = j+i){
+                v[j] = false;
+            }
+        }
+    }
+}
+
+
+int nextPrime(uint a) {
+    vector<bool> v (a+a, true);
+    garbell(v);
+    //while (!isPrime(++a));
+    while(!v[a]) ++a;
+    return a;
+}
+
 MinHashSignatures::MinHashSignatures(uint t, uint k, vector<char *> texts, vector<uint> textSize) {
     hashFunctions = vector<pair<uint, uint>>(t);
     signatures = matrix(t, vector<uint>(texts.size(), 0xFFFFFFFF));
+
     srand(time(NULL));
+
     for (uint i = 0; i < t; ++i) {
         hashFunctions[i] = pair<uint, uint>(rand(), rand());
-        cout << "H" << i << " es " << hashFunctions[i].first << "x + " << hashFunctions[i].second << " mod algo" << endl;
     }
+
     kshinglemap mapa(k);
+    unordered_set<uint> repetidos;
+    uint numRepetidos = 0;
 
     for (uint j = 0; j < texts.size(); ++j) mapa.add(j, texts[j], textSize[j]);
     cout << "kshingles total " << mapa.mapa.size() << endl;
+
+    //uint modPrime = mapa.mapa.size();
+    uint modPrime = nextPrime(mapa.mapa.size());
     int indice = 0;
+
     for (pair<uint,list<uint>> kShingleActual : mapa.mapa) {
-
         for (uint row = 0; row < t; ++row) {
-                pair<uint, uint> p = hashFunctions[row];
-                uint permutedRow = ((p.first*indice)%mapa.mapa.size() + p.second)%mapa.mapa.size();
+            pair<uint, uint> p = hashFunctions[row];
 
-                for (uint documento :  kShingleActual.second) {
-                    signatures[row][documento] = min(permutedRow, signatures[row][documento]);
-                }
+            uint permutedRow = ((p.first*indice)%modPrime + p.second)%modPrime;
+
+            if (row == 0) {
+                unordered_set<uint>::iterator it = repetidos.find(permutedRow);
+
+                if (it != repetidos.end()) ++numRepetidos;
+                else repetidos.insert(permutedRow);
+            }
+
+            for (uint documento :  kShingleActual.second) {
+                signatures[row][documento] = min(permutedRow, signatures[row][documento]);
+            }
         }
-
         ++indice;
     }
 
+    cout << "Repetidos funcion hash 1: " << numRepetidos << endl;
 
     for (uint i = 0; i < t; ++i) {
         for (uint j = 0; j < texts.size(); ++j) {
